f68a2bdba80353b3f29c65c3a52accbf4c24179f
atharatta2@gmail.com <atharatta2@gmail.com>
2023-08-30 23:01:57 +0500

    new

---
+++ assign01.js
@@ -0,0 +1,20 @@
+"use strict";
+// ADDITION :
+console.log("Hey Athar Sab, I have started learning TYPESCRIPT.");
+var username = "Athar Sab";
+console.log(username);
+var username2 = "Azhar Sab";
+console.log(username2);
+var num1 = 2;
+var num2 = 3;
+var num3 = 4;
+var num4 = 5;
+var num5 = 6;
+var num6 = 7;
+var num7 = 8;
+var num8 = 9;
+var num9 = 10;
+var num10 = 12;
+var result = num1 + num2 + num3 + num4 + num5 + num6
+    + num7 + num8 + num9 + num10;
+console.log(result);
---
+++ assign01.ts
@@ -0,0 +1,20 @@
+// ADDITION :
+
+console.log ("Hey Athar Sab, I have started learning TYPESCRIPT." )
+var username = "Athar Sab" ;
+console.log (username);
+var username2 : string = "Azhar Sab";
+console.log (username2); 
+var num1: number = 2;
+var num2: number = 3;
+var num3: number = 4;
+var num4: number = 5;
+var num5: number = 6;
+var num6: number = 7;
+var num7: number = 8;
+var num8: number = 9;
+var num9: number = 10;
+var num10: number = 12;
+var result: number = num1 + num2 + num3 + num4 + num5 + num6 
++ num7 + num8 + num9 + num10;
+console.log (result);
