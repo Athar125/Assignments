444fe726e51566b0a7b47f102b7e8295680ac940
atharatta2@gmail.com <atharatta2@gmail.com>
2023-08-31 11:00:25 +0500

    new folder

---
+++ assign02.ts
@@ -0,0 +1,91 @@
+/*
+
+console.log ("Hey Athar Sab, I have started learning TYPESCRIPT." )
+var username = "Athar Sab" ;
+console.log (username);
+
+//Addition
+var num1:number = 20;
+var num2:number = 25;
+var result:number = num1 + num2;
+console.log(result);
+
+//Substraction
+var num3:number = 45;
+var num4:number = 23;
+var result:number = num3 - num4;
+console.log(result);
+
+//Multiply
+var num5:number = 34;
+var num6:number = 45;
+var result:number = num5 * num6;
+console.log(result);
+
+//Divide
+var num7:number = 124;
+var num8:number = 8;
+var result:number = num7 / num8;
+console.log(result);
+
+//Exponentions
+var $1:number = 4;
+var $2:number = 2;
+var result:number = $1 ** $2;
+console.log(result);
+
+//Modules
+var Per1:number = 1;
+var Per2:number = 22;
+var result:number = Per1 % Per2;
+console.log(result);
+
+//Assignment operator
+let z = 5;
+console.log(z);
+
+var num1:number = 10;
+num1 += 5;
+console.log(num1)
+
+// Comparison operators
+var $a3:number = 34;
+var $a4:number = 23;
+var output:boolean = $a3 < $a4;
+console.log(output);
+
+// Logical Operators
+var _$f1 = 18;
+var _$f2 = 15;
+var output:boolean = 55==55 && _$f1 ==_$f2; 
+console.log(output);
+
+var nb3:number = 34;
+var nb4:number = 32;
+var ans:boolean = 33==33 && nb3 == nb4;
+console.log(ans);
+
+var num1:number = 557;
+var num2:number = 334;
+var ans:boolean = num1 == num1 || num1 == num2;
+console.log(ans);
+
+var userName:string = "ATHAR ATTA";
+var age:number = 28;
+var nationality:string = "PAKISTANI";
+var answer:boolean = age>28 && nationality == "PAKISTANI";
+console.log(answer);
+
+//All Arithmatic Operators
+var result2 = num1 + num4 - num6 * num8 / $2 ** Per1 % _$f1;
+console.log(result2);
+
+// Type of Variables
+var userNum:number = 125;
+console.log(typeof userNum);
+
+//Template Literals
+var myName:string = "ATHAR ATTA";
+var myName2:string = 'AZHAR ATTA $ {myName}';
+console.log(myName2);
+*/
