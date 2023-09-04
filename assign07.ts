fb8e90ec1da186aa748e324083f4eb4fc0ab920f
atharatta2@gmail.com <atharatta2@gmail.com>
2023-09-04 23:54:24 +0500

    new

---
+++ assign07.ts
@@ -0,0 +1,9 @@
+// 02. Personal Message: Store a person’s name in a variable, and print a message to that person. 
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let variableA = "Hello Athar, How are you.";
+console.log(variableA); 
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
+// uppercase, and titlecase.
+
