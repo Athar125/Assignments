791ef35e67e197ca8da3cf9b3343d16a989a26fe
atharatta2@gmail.com <atharatta2@gmail.com>
2023-09-07 16:33:14 +0500

    new

--- assign07.ts
+++ assign07.ts
@@ -1,8 +1,8 @@
 // 02. Personal Message: Store a person’s name in a variable, and print a message to that person. 
 // Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 /*
-let variableA = "Hello Athar, How are you.";
-console.log(variableA); 
+const userName = "ATHAR";
+console.log("Hello", userName, "Would you like to learn some Python today?");
 */
 // 03. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
 // uppercase, and titlecase.
@@ -11,10 +11,11 @@ var name1:string="Athar Sab";
 console.log(name1.toLowerCase());
 console.log(name1.toUpperCase())
 */
+
 // 05. Famous Quote: Find a quote from a famous person you admire. Print the 
 // quote and the name of its author. Your output should look something 
 // like the following, including the quotation marks:
-
+/*
 const quote:string= "No legacy is so rich as honesty" ;
 const quote1:string="William Shakespeare";
 console.log(quote+":",quote1);
@@ -31,6 +32,7 @@ console.log(quote+":",quote2);
 // Make sure you use each character combination, "\t" and "\n", at least once. 
 // Print the name once, so the whitespace around the name is displayed. 
 // Then print the name after striping the white spaces.
+
 var stripName:string="Muha \t mmad \n Athar";
 console.log(stripName);
 
@@ -56,19 +58,25 @@ console.log(f/b);
 
 let favNum:number = 10;
 let favNum1:string = "MY FAVOURITE NUMBER IS 10"
-
 console.log("favNum", {favNum1})
-
 
 // 10. Adding Comments: Choose two of the programs you’ve written, and add at least one 
 // comment to each. If you don’t have anything specific to write because your 
 // programs are too simple at this point, just add your name and the current date 
 // at the top of each program file. Then write one sentence describing what the 
 // program does.
+/*
+let favNum:number = 10;
+let favNum1:string = "MY FAVOURITE NUMBER IS 10"
+console.log("favNum", {favNum1})
 
 
+// 11. Names: Store the names of a few of your friends in a array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
 
-
-
+let friendsName = ["ATHAR", "AZHAR", "SHADAB", "AFTAB", "SAHIL", "AFTAB ", "AKASH"];
+for (let index = 0; index < friendsName.length; index++) 
+    console.log(friendsName[index]);
+*/
 
 
