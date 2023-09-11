6cd6c95bdf3dcd2fd78c2970745e3ae7a87edfed
atharatta2@gmail.com <atharatta2@gmail.com>
2023-09-11 07:54:57 +0500

    assignment

--- assign07.ts
+++ assign07.ts
@@ -410,4 +410,144 @@ for (const username of usernames) {
         console.log(`Hello ${username}, thank you for logging in again.`);
     }
 }
+
+// 31. No Users: Add an if test to Exercise --28 to make sure the list of users is not empty.
+let newUsers = ["salman ", "raza", "admin", "ahad", "rizwan"];
+if (newUsers.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    for (let newUser of newUsers) {
+        if (newUser === 'admin') {
+            console.log("Hello admin, would you like to see a status report?");
+        }
+        else {
+            console.log(`Hello ${newUser}, thank you for logging in again.`);
+        }
+    }
+}
+
+// 32. Remove all of the usernames from your array, and make sure the correct message is printed.
+// Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+let currentUsers = ["mian", "umar", "butt", "rana", "jutt"];
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+let newUser = ["ali", "hamza", "hassan", "khan", "ibrahim"];
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+const currentUsersLower = currentUsers.map(user => user.toLowerCase());
+for (const newUsers of newUser) {
+    if (currentUsersLower.includes(newUsers.toLowerCase())) {
+        console.log(`Sorry, the username '${newUsers}' is already taken. Please choose a different one.`);
+    }
+    else {
+        console.log(`The username '${newUsers}' is available.`);
+    }
+}
+
+// 33. Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.             
+//  • Store the numbers 1 through 9 in a array.
+let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let index = 0; index <= numArray.length; index++) {
+    if (index === 1) {
+        console.log(`${index}st`);
+    }
+    else if (index === 2) {
+        console.log(`${index}nd`);
+    }
+    else if (index === 3) {
+        console.log(`${index}rd`);
+    }
+    else if (index === 4) {
+        console.log(`${index}th`);
+    }
+    else if (index === 5) {
+        console.log(`${index}th`);
+    }
+    else if (index === 6) {
+        console.log(`${index}th`);
+    }
+    else if (index === 7) {
+        console.log(`${index}th`);
+    }
+    else if (index === 8) {
+        console.log(`${index}th`);
+    }
+    else if (index === 9) {
+        console.log(`${index}th`);
+    }
+}
+
+
+// 34. Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then 
+// use a for loop to print the name of each pizza.
+let favPizza = ["BBQ", "bhari kabab", "mali Boti", "Fajita"];
+for (let index = 0; index < favPizza.length; index++) {
+    console.log(favPizza[index]);
+}
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza.
+// For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+for (let index = 0; index < favPizza.length; index++) {
+    console.log(`I Like ${favPizza[index]} pizza`);
+}
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza.
+//  The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+console.log("I really love pizza!");
+
+// 35. Animals: Think of at least three different animals that have a common characteristic. Store the names of these 
+// animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. •
+// Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+let animals = ["Cat", "cow", "goat"];
+for (const animal of animals) {
+    if (animal === "Cat") {
+        console.log(`A ${animal} would make a great pet.`);
+    }
+    else if (animal === "cow") {
+        console.log(`A ${animal} used to get Milk.`);
+    }
+    else if (animal === "goat") {
+        console.log(`A ${animal} is alos used to get Milk.`);
+    }
+    ;
+}
+console.log("what these animals have in common. ");
+console.log("these animal could ba a great pet!");
+
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+function make_shirt(size:string, message:string) {
+    console.log(`you order ${size} T-shirt and message  you want to print is "${message}"`);
+}
+make_shirt("large", "ATHAR 10");
+
+// 37. Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+function makeShirt(size:string) {
+    if (size === `large`) {
+        console.log("I love TypeScript.");
+    }
+    else if (size === `medium`) {
+        console.log("I love Pakistan.");
+    }
+    else if (size === `small`) {
+        console.log("I LOVE IRSA.");
+    }
+}
+makeShirt("large");
+makeShirt("medium");
+makeShirt("small");
+
+// 38. Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
+function describe_city(city:string, country = `default COuntry`) {
+    console.log(`${city} is in ${country}`);
+}
+describe_city("lahore", "pakistan");
+describe_city("istanbol", "turkey");
+describe_city("new dehli"); //with defaultcountry
+
+// 39. City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+"Lahore, Pakistan" Call your function with at least three city-country pairs, and print the value that’s returned.
+function city_country(city:string, country:string) {
+    console.log(`"${city}, ${country}"`);
+}
+city_country("Faislabad", "Pakistan");
+city_country("Istanbul", "Turkey");
+city_country("Beijing", "China");
 */
